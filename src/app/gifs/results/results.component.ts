ee513cfae124264e9658803365fd0fa75105c648
unknown <lamica.olivares@gmail.com>
2022-05-27 10:44:57 -0400

    servicio-api_key-module

--- src/app/gifs/results/results.component.ts
+++ src/app/gifs/results/results.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-results',
@@ -6,11 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class ResultsComponent implements OnInit {
+export class ResultsComponent  {
 
-  constructor() { }
+get resultados() {
+  return this.gifsService.resultado;
+}
+  //inyectar el servicio
+  constructor( private gifsService: GifsService) {
 
-  ngOnInit(): void {
   }
+  
 
 }
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,17 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
+ 
+ 
+  private apiKey: string = 'OVDmKTCFm2JtKNPKnwUsjIyE0FUW4hLq';
 
-private _historial: string[] = [];
+  private _historial: string[] = [];
 
+  public resultado : any[] = [];
+ 
 get historial(){
  // this._historial = this._historial.splice(0, 10);
   //[...romper la referencia y retornar un nuevo arreglo]
   return [...this._historial];
 }
+
+constructor(private http: HttpClient) {
+
+}
+
 buscarGifs( query : string){
 
 query = query.trim().toLowerCase();
@@ -23,9 +34,18 @@ query = query.trim().toLowerCase();
   }
 
   this._historial = this._historial.splice(0, 10);
-  
-  console.log(this._historial);
-}
+   console.log(this._historial);
+
+
+   this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=OVDmKTCFm2JtKNPKnwUsjIyE0FUW4hLq&q=${query}&limit=10`)
+   .subscribe((resp : any) => {
+   console.log(resp.data);
+  this.resultado = resp.data; 
+  });
+   
+
+  }
+
 
 }
 
