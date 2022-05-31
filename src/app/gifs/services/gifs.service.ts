814e14fe7fb45dca9f3324ab204fbcc835f744c3
unknown <lamica.olivares@gmail.com>
2022-05-31 11:02:48 -0400

    persistencia de imagenes y titulos

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interfaces/interfacesGifs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +13,7 @@ export class GifsService {
 
   private _historial: string[] = [];
 
-  public resultado : any[] = [];
+  public resultado : Gif[] = [];
  
 get historial(){
  // this._historial = this._historial.splice(0, 10);
@@ -20,6 +22,12 @@ get historial(){
 }
 
 constructor(private http: HttpClient) {
+
+//this._historial = localStorage.getItem("historial");
+
+  this._historial = JSON.parse(localStorage.getItem("historial")!) || [];
+  this.resultado = JSON.parse(localStorage.getItem("resultado")!) || [];
+
 
 }
 
@@ -31,16 +39,21 @@ query = query.trim().toLowerCase();
 //primero hago la comprovacion si no lo incluye lo inserto
   if(!this._historial.includes(query)){
     this._historial.unshift(query);
-  }
+    this._historial = this._historial.splice(0, 10);
 
-  this._historial = this._historial.splice(0, 10);
+    localStorage.setItem("historial", JSON.stringify( this._historial));
+
+  }
    console.log(this._historial);
 
-
-   this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=OVDmKTCFm2JtKNPKnwUsjIyE0FUW4hLq&q=${query}&limit=10`)
-   .subscribe((resp : any) => {
+//la respuesta luce como la interface SearchGifsResponse
+   this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=OVDmKTCFm2JtKNPKnwUsjIyE0FUW4hLq&q=${query}&limit=10`)
+   .subscribe((resp ) => {
    console.log(resp.data);
   this.resultado = resp.data; 
+
+  localStorage.setItem("resultado", JSON.stringify( this.resultado));
+  
   });
    
 
